9b4bd1fff8dea3c9798b51b92be38f14805f256c
Nate Schieber <nbschieber@gmail.com>
2024-06-05 20:01:41 -0700

    wip: update seed-data

--- track/src/main/resources/sql/seed-data.sql
+++ track/src/main/resources/sql/seed-data.sql
@@ -8,5 +8,6 @@ VALUES ('Speak Now', '2010-10-25'),
        ('Random Access Memories', '2013-5-13');
 
 INSERT INTO track (title, issue_date, duration_in_seconds, track_media_type, language, artist_id, album_id)
-VALUES ('Enchanted', '2010-10-25', 240, 1, 'English', 1, 1);
+VALUES ('Enchanted', '2010-10-25', 240, 1, 'English', 1, 1),
+       ('Doin'' It Right', '2013-5-13', 320, 2, 'English', 2, 2);
 
